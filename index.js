37a5b547061825f4b5684f23fd9e0ceaaa14d545
xdh-git <xudanhui123@gmail.com>
2023-01-12 21:26:40 +0800

    23.1.12

---
+++ index.js
@@ -0,0 +1,6 @@
+var fullDate = new Date();
+var date = fullDate.getDate(); // %D
+var day = fullDate.getDay(); // %d
+var year = fullDate.getFullYear(); // %y
+var hours = fullDate.getHours(); // %h
+var month = fullDate.getMonth(); // %m
